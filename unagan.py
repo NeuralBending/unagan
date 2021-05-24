19010598151e4e34e8492c47515f68b03ac7d8a3
NeuralBending <75141144+NeuralBending@users.noreply.github.com>
2021-05-24 15:54:42 +0200

    gpu<->cpu

--- unagan.py
+++ unagan.py
@@ -11,7 +11,7 @@ z_dim = 20
 z_scale_factors = [2, 2, 2, 2]
 z_total_scale_factor = np.prod(z_scale_factors)
 
-def unagan(data_type = 'singing'):
+def unagan(data_type = 'singing', gpu=0):
   vocoder_dir = f'models/{data_type}/vocoder/'
   vocoder_config_fp = os.path.join(vocoder_dir, 'args.yml')
   vocoder_config = read_yaml(vocoder_config_fp)
@@ -22,8 +22,9 @@ def unagan(data_type = 'singing'):
 
   mean = torch.from_numpy(np.load(mean_fp)).float().view(1, feat_dim, 1)
   std = torch.from_numpy(np.load(std_fp)).float().view(1, feat_dim, 1)
-  mean = mean.cuda(0)
-  std = std.cuda(0)
+  if gpu>=0:
+    mean = mean.cuda(0)
+    std = std.cuda(0)
 
   n_mel_channels = vocoder_config.n_mel_channels
   ngf = vocoder_config.ngf
@@ -32,7 +33,8 @@ def unagan(data_type = 'singing'):
   ## Generator
   generator = HierarchicalGenerator(n_mel_channels, z_dim, z_scale_factors)
   manager.load_model(param_fp, generator, device_id='cpu')
-  generator = generator.cuda(0)
+  if gpu>=0:
+    generator = generator.cuda(0)
   generator.eval()
   for p in generator.parameters():
     p.requires_grad = False
@@ -50,5 +52,6 @@ def unagan(data_type = 'singing'):
   vocoder.eval()
   vocoder_param_fp = os.path.join(vocoder_model_dir, 'params.pt')
   vocoder.load_state_dict(torch.load(vocoder_param_fp))
-  vocoder = vocoder.cuda(0)
+  if gpu>=0:
+    vocoder = vocoder.cuda(0)
   return generator, vocoder, mean, std
